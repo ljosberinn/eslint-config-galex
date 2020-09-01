0f1d86c6d92dd148c9f4f6428300c97bc7791186
Gerrit Alex <admin@gerritalex.de>
2020-09-02 00:29:23 +0200

    feat: v1
    BREAKING CHANGE: everything changed

--- src/createConfig.js
+++ src/createConfig.js
@@ -67,7 +67,7 @@ const parserOptions = {
 const getDependencies = ({ cwd = process.cwd() } = {}) => {
   // adapted from https://github.com/kentcdodds/eslint-config-kentcdodds/blob/master/jest.js
   try {
-    /* istanbul ignore next line 75 is supposedly uncovered */
+    /* istanbul ignore next line 75 is supposedly uncovered :shrug: */
     const {
       packageJson: {
         peerDependencies = {},
