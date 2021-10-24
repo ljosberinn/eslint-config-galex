c9aefe613c6421a87166ea4dbef14dbf20bf067f
Gerrit Alex <admin@gerritalex.de>
2021-10-24 22:38:44 +0200

    fix: disable eslint-plugin-react-hooks until compatible with ESLint v8

--- src/overrides/react.js
+++ src/overrides/react.js
@@ -66,7 +66,7 @@ const createReactOverride = ({
     ...createNextJsRules({ react }),
     ...prettierReactRules,
     ...createJSXA11yRules({ react }),
-    ...hookRules,
+    // ...hookRules,
     ...customRules,
   };
 
