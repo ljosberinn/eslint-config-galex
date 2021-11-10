9f7c83cbd8f03206af7d3b143828ed526f2eadf7
Gerrit Alex <admin@gerritalex.de>
2021-11-10 11:54:53 +0100

    chore: debug test

--- src/__tests__/integration.test.js
+++ src/__tests__/integration.test.js
@@ -2,7 +2,7 @@
 // @ts-check
 
 const { execSync } = require('child_process');
-const { readFileSync, writeFileSync } = require('fs');
+const { readFileSync } = require('fs');
 const { resolve } = require('path');
 
 const env = {
@@ -21,9 +21,11 @@ const env = {
 const readSnapshots = cwd => {
   try {
     return {
-      results: readFileSync(resolve(cwd, 'results.txt'), {
-        encoding: 'utf-8',
-      }),
+      results: normalizeSnapshot(
+        readFileSync(resolve(cwd, 'results.txt'), {
+          encoding: 'utf-8',
+        })
+      ),
       config: require(resolve(cwd, 'eslint-config.json')),
       deps: require(resolve(cwd, 'deps.json')),
     };
@@ -40,17 +42,11 @@ const variablePathDelimiter = 'eslint-config-galex';
 
 /**
  *
- * @param {string} cwd
+ * @param {string} content
  */
-const normalizeSnapshot = cwd => {
-  const resultFilePath = resolve(cwd, 'results.txt');
-
-  writeFileSync(
-    resultFilePath,
-    readFileSync(resultFilePath, {
-      encoding: 'utf-8',
-    })
-      // iterate each line in results.txt
+const normalizeSnapshot = content => {
+  return (
+    content // iterate each line in results.txt
       .split('\n')
       // skip yarn start/end
       .filter(
@@ -76,19 +72,6 @@ const normalizeSnapshot = cwd => {
   );
 };
 
-const defaultArgs = Object.entries({
-  'max-warnings': 0,
-  format: 'tap > results.txt',
-}).reduce((acc, [key, value]) => {
-  const next = `--${key}=${value}`;
-  return acc ? [acc, next].join(' ') : next;
-}, '');
-
-const foldersToLint = {
-  cra: 'src',
-  next: 'pages',
-};
-
 const cases = [
   { name: 'create-react-app javascript', type: 'cra', lng: 'js' },
   { name: 'create-react-app typescript', type: 'cra', lng: 'ts' },
@@ -97,15 +80,13 @@ const cases = [
 ];
 
 describe.each(cases)('$case.name', ({ type, name, lng }) => {
-  const cmd = `yarn lint:integration ${foldersToLint[type]} ${defaultArgs}`;
   const cwd = resolve('integration', `${type}-${lng}`);
   const { results, config, deps } = readSnapshots(cwd);
 
   test(`${name}`, () => {
     try {
-      execSync(cmd, { cwd, env });
+      execSync('yarn lint:integration', { cwd, env });
     } catch {
-      normalizeSnapshot(cwd);
       const updatedSnapshots = readSnapshots(cwd);
 
       if (results) {
