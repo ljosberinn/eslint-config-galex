c22b90106fe138ee0fb98a85d61c32f9cacf6f42
Muamet Islami <42146933+muametgrooby@users.noreply.github.com>
2022-01-17 18:55:07 +0100

    fix: disable no-extraneous-class in nestjs

--- src/overrides/typescript.js
+++ src/overrides/typescript.js
@@ -1060,6 +1060,7 @@ const getNestJsRules = ({ hasNest }) => {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-extraneous-class': 'off',
   };
 };
 
